e677c7f56c80c04cb4497e4f87726a7c2f134fe0
Pieris128 <elpichichi2048@gmail.com>
2022-10-23 17:45:21 -0300

    Aparently working now

---
+++ PrimeraAplicacion/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrimeraAplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola chiques!"); 
+           
+        }
+    }
+}
